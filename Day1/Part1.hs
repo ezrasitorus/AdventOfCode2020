8a199a16776f81ab7c56610cca5d3e48aedf1070
es1419 <es1419@ic.ac.uk>
2020-12-01 12:06:29 +0000

    Day 1

---
+++ Day1/Part1.hs
@@ -0,0 +1,17 @@
+-- import System.IO
+
+convert :: String -> [Int]
+convert = map read . words
+
+getResult :: [Int] -> Int
+getResult [] = error "Values not found"
+getResult (n:ns)
+    | complement `elem` ns = complement * n
+    | otherwise = getResult ns
+    where
+        complement = 2020 - n
+
+main :: IO ()
+main = do
+    myFile <- readFile "input.txt"
+    print $ (getResult . convert) myFile
