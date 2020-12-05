31ec001d7d1593dfcaba9787a9329b6e01b5d59b
es1419 <sitorusezra@outlook.com>
2020-12-05 11:03:05 +0000

    Formatted

--- Day1/Part2.hs
+++ Day1/Part2.hs
@@ -5,22 +5,21 @@ convert = map read . words
 
 getResult :: [Int] -> Int
 getResult [] = error "Values not found"
-getResult (n:ns)
-    | isNothing res = getResult ns
-    | otherwise = n * fromJust res
-    where
-        complement = 2020 - n
-        res = getResult' ns complement
+getResult (n : ns)
+  | isNothing res = getResult ns
+  | otherwise = n * fromJust res
+  where
+    complement = 2020 - n
+    res = getResult' ns complement
 
 getResult' :: [Int] -> Int -> Maybe Int
 getResult' [] _ = Nothing
 getResult' [n] _ = Nothing
-getResult' (n:ns) c
-    | (c - n) `elem` ns = Just (n * (c - n))
-    | otherwise = getResult' ns c
-
+getResult' (n : ns) c
+  | (c - n) `elem` ns = Just (n * (c - n))
+  | otherwise = getResult' ns c
 
 main :: IO ()
 main = do
-    myFile <- readFile "input.txt"
-    print $ (getResult . convert) myFile
+  myFile <- readFile "input.txt"
+  print $ (getResult . convert) myFile
