0e9fe6671df7226cc44f0d184224be0f5b149376
es1419 <sitorusezra@outlook.com>
2020-12-05 11:03:12 +0000

    Formatted

--- Day2/Part1.hs
+++ Day2/Part1.hs
@@ -2,15 +2,15 @@ parseString :: String -> [String]
 parseString = words . map (\c -> if c == ':' || c == '-' then ' ' else c)
 
 check :: [String] -> Bool
-check [s,e,[c],str] = size >= s' && size <= e'
-    where
-        size = length $ filter (== c) str
-        [s', e'] = map read [s, e]
+check [s, e, [c], str] = size >= s' && size <= e'
+  where
+    size = length $ filter (== c) str
+    [s', e'] = map read [s, e]
 
 count :: String -> Int
 count str = length $ filter check $ map parseString $ lines str
 
 main :: IO ()
 main = do
-    passwords <- readFile "input.txt"
-    print $ count passwords
+  passwords <- readFile "input.txt"
+  print $ count passwords
--- Day2/Part2.hs
+++ Day2/Part2.hs
@@ -2,16 +2,16 @@ parseString :: String -> [String]
 parseString = words . map (\c -> if c == ':' || c == '-' then ' ' else c)
 
 check :: [String] -> Bool
-check [s,e,[c],str] 
-    | length str < e' = False
-    | otherwise = (str !! (s' - 1) == c) /= (str !! (e' - 1) == c)
-    where
-        [s', e'] = map read [s, e]
+check [s, e, [c], str]
+  | length str < e' = False
+  | otherwise = (str !! (s' - 1) == c) /= (str !! (e' - 1) == c)
+  where
+    [s', e'] = map read [s, e]
 
 count :: String -> Int
 count str = length $ filter check $ map parseString $ lines str
 
 main :: IO ()
 main = do
-    passwords <- readFile "input.txt"
-    print $ count passwords
+  passwords <- readFile "input.txt"
+  print $ count passwords
