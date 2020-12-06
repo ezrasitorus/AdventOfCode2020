999304402961c9e41ac5263530ce119ddbadffd5
es1419 <sitorusezra@outlook.com>
2020-12-06 14:11:38 +0000

    Added another implementation of intersection

--- Day6/Part2.hs
+++ Day6/Part2.hs
@@ -10,8 +10,13 @@ convert = paragraphs . lines
 intersection :: [String] -> String
 intersection = foldl1 intersect
 
+-- A potentially quicker way of doing intersection
+intersection' :: [String] -> String
+intersection' [s] = s
+intersection' (s:ss) = filter (\c -> all (elem c) ss) s
+
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    print $ (sum . map (length . intersection) . convert) input
+    print $ (sum . map (length . intersection') . convert) input
     
